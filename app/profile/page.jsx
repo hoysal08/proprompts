4a8dba5197c5be4d829b82d4cfbef55ca7e76574
hoysal08 <suraj_yahoo.com>
2023-05-07 16:19:29 +0530

    My profile page completed

---
+++ app/profile/page.jsx
@@ -0,0 +1,41 @@
+"use client"
+
+import React from 'react'
+import { useState,useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router' 
+import Profile from '@components/Profile'
+
+const MyProfile = () => {
+
+
+    const {data:session}=useSession();
+    const[posts,setPosts]=useState([])
+
+    const handleEdit= async ()=>{
+
+    }
+    const handleDelete= async ()=>{
+
+    }
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+          const response = await fetch(`api/users/${session?.user.id}/posts`);
+          const data = await response.json();
+          setPosts(data);
+        };
+       if(session?.user.id) fetchPosts();
+      }, []);
+
+  return (
+    <Profile name="My"
+    desc="Welcome to you personalized profile page"
+    data={posts}
+    handleEdit={handleEdit}
+    handleDelete={handleDelete}
+    />
+  )
+}
+
+export default MyProfile
