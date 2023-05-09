4edc31c6dbf19240f8962828b513a5b5bf47f83f
hoysal08 <suraj_yahoo.com>
2023-05-09 11:54:08 +0530

    Added placeholder to search button

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -63,7 +63,7 @@ const Feed = () => {
       <form className="relative w-full flex-center">
         <input
           type="text"
-          placeholder="Search for a tag or a username"
+          placeholder="Search for a tag or a username (i.e - interviewer)"
           value={searchText}
           onChange={handlesearchchange}
           onPaste={handlesearchchange}
